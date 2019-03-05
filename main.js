3a7b6b593de8e815d8c253dc36be1b2284cbc395
MissionBit <missionbit@81p-MissionBit.local>
2019-03-04 17:52:09 -0800

    created a new code and started to make questions

---
+++ main.js
@@ -0,0 +1,7 @@
+window.prompt('What is your name?')
+window.prompt('What is your favorite color?')
+window.prompt('Give an adjective that describes yourself ')
+window.prompt('Give a noun')
+let element = document.getElementById('content');
+element.innerText = 'hello';
+
