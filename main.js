544a6d251bd3dde77afd25dabbef97f34a8b8001
MissionBit <missionbit@81p-MissionBit.local>
2019-03-11 16:53:35 -0700

    added info and connected to the java

--- main.js
+++ main.js
@@ -2,9 +2,27 @@
 let feeling = document.getElementById('feeling')
 feeling.innerText=window.prompt('What is your feeling?')
 
+feeling = document.getElementById('Creation')
+creation.innerText=window.prompt('What it your creation?')
+
+adjective = document.getElementById('adjective')
+adjective.innerText=window.prompt('What is your adjective?')
+
+
 let button = document.getElementById('change-creation-button');
 function changecreation() {
     let creation = document.getElementById('creation');
     creation.innerText=window.prompt('What is your creation?');
 }
+
+feeling = document.getElementById('change-feeling-button');
+function changefeelings() {
+    let creation = document.getElementById('What is your feeling?')
+}
+
+
 button.onclick=changecreation;
+button.onclick=changecreation;
+
+
+
