7c6ee483379a823fe0b4ff9fbb615d4821cf28e5
MissionBit <missionbit@81p-MissionBit.local>
2019-03-13 17:19:16 -0700

    the demo that went on class

---
+++ svg-demo.js
@@ -0,0 +1,11 @@
+ console.log('loaded svg-demon.js');
+
+ let draw = SVG('drawing'). size (480,480);
+let rect = draw.rect(100,100).fill('purple')
+
+let x= 0;
+let y= 0;
+rect.move 
+
+document.getElementById ().onclick = function() 
+console.log('moving rectangle') 
