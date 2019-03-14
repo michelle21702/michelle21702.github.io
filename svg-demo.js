e237a2e04faf0f3cd6c5085679116c08bc25c206
MissionBit <missionbit@81p-MissionBit.local>
2019-03-13 17:28:20 -0700

    whoops

--- svg-demo.js
+++ svg-demo.js
@@ -2,10 +2,17 @@
 
  let draw = SVG('drawing'). size (480,480);
 let rect = draw.rect(100,100).fill('purple')
+iv id="drawing">
+    <svg xmlnbs="http://www.w3.org/2000/svg" version="1.1"
+ xmlns:xlink="http://www.w3.org/1999/xlink" width="300" heigth="300">
+    <rect width="100" fill="#f06"></rect>
+ </svg>
+ </div>
+ var draw = SVG('drawing').size('100', '100%')
 
-let x= 0;
-let y= 0;
-rect.move 
-
-document.getElementById ().onclick = function() 
-console.log('moving rectangle') 
+if (SVG.supported){
+    var draw = SVG('drawing')
+    var rect = draw.rect(100,100)
+} else {
+    alert('SVG not supported')
+}
