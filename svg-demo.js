b7ddec5e2f735b97558ff419540fd337506a84df
MissionBit <missionbit@81p-MissionBit.local>
2019-03-18 17:56:25 -0700

    flower petal #1

--- svg-demo.js
+++ svg-demo.js
@@ -1,1 +1,5 @@
-var draw = SVG('drawing').size (300,150)
+console.log('loaded svg-demo.js');
+var draw = SVG('drawing').size(300, 130);
+var ellipse = draw.ellipse(150,100).fill(' #ffffb3').move(30,50)
+
+
